c458309abe0f99995a7faacc2c2753ec118d5cd3
jasperjessie <JASPERJESSIE@GMAIL.COM>
2026-03-18 19:08:30 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  //name: 'Next.js Notion Starter Kit',
-  //domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Jasper Jessie',
 
   // open graph metadata (optional)
